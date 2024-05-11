ce01937aeab3daada70b7ab9e81b80fac86427f1
cache6 <cache6.dev@gmail.com>
2024-05-11 15:30:08 +0900

    Revert "shop 검색 기능 만들어보기"
    This reverts commit 111ee9b731f99ee2bcfd77988d42931ac1425e6f.

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState, useCallback, Suspense, useEffect } from 'react';
+import React, { useState, useCallback, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { Button } from "@/components/atoms/button"
 import ProductList from "@/components/organisms/productList"
@@ -19,14 +19,6 @@ function KeywordDisplay() {
 
 export default function ShopPage() {
     const [, setInputValue] = useState('');
-    const searchParams = useSearchParams();
-    const [keyword, setKeyword] = useState('');
-
-    useEffect(() => {
-        // URL의 검색어가 변경될 때마다 keyword 상태를 업데이트
-        const newKeyword = searchParams.get('keyword') || '';
-        setKeyword(newKeyword);
-    }, [searchParams]); // searchParams가 변경될 때마다 useEffect 실행
 
     const handleInputValue = (value: string) => {
         setInputValue(value);
@@ -35,8 +27,7 @@ export default function ShopPage() {
     const [resetKey, setResetKey] = useState(0);
     const clearFilters = useCallback(() => {
         setResetKey(prevKey => prevKey + 1);
-        setKeyword(''); // 필터를 클리어할 때 검색어도 초기화
-    }, [searchParams]);
+    }, []);
 
     return (
         <>
@@ -57,7 +48,7 @@ export default function ShopPage() {
                 <div className='flex-1'>
                     <ProductCategories />
                     <div className="pt-8">
-                        <ProductList keyword={keyword} />
+                        <ProductList />
                     </div>
                 </div>
             </div>
--- src/components/organisms/productList.tsx
+++ src/components/organisms/productList.tsx
@@ -1,39 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import ProductComponent from '@/components/atoms/product';
+import React, { useState } from 'react';
+import Product from '@/components/atoms/product';
 import { Button } from '@/components/atoms/button';
-import { Product } from '@/entities/product';
 
-interface ProductListProps {
-    keyword: string;
-}
-
-const ProductList: React.FC<ProductListProps> = ({ keyword }) => {
+const ProductList = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const itemsPerPage = 8;
-    const [filteredData, setFilteredData] = useState<Product[]>([]);
 
-    useEffect(() => {
-        const dummyData = Array.from({ length: 10 }, (_, index) => ({
-            id: index + 1,
-            productImageUrl: `/images/cat_3.png`,
-            title: `제목${index + 1}`,
-            externalNote: `상세 설명${index + 1}`,
-            tag: `태그${index + 1}`,
-            price: 1000 * (index + 1),
-            isRecommended: index % 2 === 0,
-            category: { id: 1, name: "Category 1", categoryImageUrl: "/images/cat_category.png" }
-        }));
-
-        const newFilteredData = dummyData.filter(item => item.title.toLowerCase().includes(keyword.toLowerCase()));
-        setFilteredData(newFilteredData);
-        console.log(newFilteredData.length); // 필터링된 데이터의 길이를 로그로 출력
-    }, [keyword]); // keyword가 변경될 때마다 useEffect 실행
+    // 더미 데이터 생성
+    const dummyData = Array.from({ length: 10 }, (_, index) => ({
+        id: index + 1,
+        productImageUrl: `/images/cat_3.png`, // 이미지 URL 수정
+        name: `제품명${index + 1}`,
+        title: `제목${index + 1}`,
+        detail: `상세 설명${index + 1}`,
+        tag: `태그${index + 1}`,
+        price: 1000 * (index + 1),
+        isRecommended: index % 2 === 0,
+        category: { id: 1, name: "Category 1", categoryImageUrl: "/images/cat_category.png" }
+    }));
 
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
+    const currentItems = dummyData.slice(indexOfFirstItem, indexOfLastItem);
 
-    const totalPages = Math.ceil(filteredData.length / itemsPerPage);
+    const totalPages = Math.ceil(dummyData.length / itemsPerPage);
     const renderPageNumbers = Array.from({ length: totalPages }, (_, i) => (
         <Button key={i + 1} onClick={() => setCurrentPage(i + 1)}>
             {i + 1}
@@ -42,10 +32,15 @@ const ProductList: React.FC<ProductListProps> = ({ keyword }) => {
 
     return (
         <>
-            <div className='flex flex-row flex-wrap gap-8 min-h-[640px]'>
+            <div className='flex flex-row flex-wrap gap-8 min-h-[640px]'> {/* Adjust minHeight based on your item height */}
                 {currentItems.map((item, index) => (
                     <div key={index} className='flex flex-col w-1/5'>
-                        <ProductComponent product={item} />
+                        <Product product={item} />
+                    </div>
+                ))}
+                {Array.from({ length: 8 - currentItems.length }, (_, i) => (
+                    <div key={i} className='flex flex-col w-1/5 invisible'>
+                        <Product product={dummyData[0]} />
                     </div>
                 ))}
             </div>
@@ -57,3 +52,4 @@ const ProductList: React.FC<ProductListProps> = ({ keyword }) => {
 }
 
 export default ProductList;
+
